5eaf22c7b23a968ec6ba35a3230fe24d9e8eede0
Saptarshe <saptarshe.sinha@gmail.com>
2023-11-01 13:38:28 +0530

    Backend upadated

--- backend/index.js
+++ backend/index.js
@@ -14,13 +14,13 @@ const file = require("./FileOperations");
 const app = express()
 const port = process.env.PORT || 3000;        // Using env variable
 
-app.use(cors({
-    origin: 'https://code-pro-one.vercel.app',    // Replace with your frontend's origin
-    methods: 'POST, DELETE',       // Allow only POST requests from the frontend
-    credentials: true,
-  }));
+// app.use(cors({
+//     origin: 'https://code-pro-one.vercel.app',    // Replace with your frontend's origin
+//     methods: 'POST, DELETE',       // Allow only POST requests from the frontend
+//     credentials: true,
+//   }));
 
-// app.use(cors())
+app.use(cors())
 
 app.use(bodyParser.json());
 
